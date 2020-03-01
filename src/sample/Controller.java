c34c37be0a0fb87aa2678fbcdd5f884fc4d4b608
Denis <denisbaic@yandex.ru>
2020-03-01 23:36:06 +0300

    Added Intersection score, checkboxes

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,21 +5,26 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.beans.InvalidationListener;
 import javafx.beans.property.FloatProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleFloatProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Bounds;
 import javafx.geometry.Point2D;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
+import jdk.jfr.Enabled;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -33,14 +38,22 @@ public class Controller {
     private Rectangle ENEMY1;
     private Enemy En1;
     @FXML
+    private CheckBox ENEMY1_ENABLE;
+    @FXML
     private Rectangle ENEMY2;
     private Enemy En2;
+    @FXML
+    private CheckBox ENEMY2_ENABLE;
     @FXML
     private Rectangle ENEMY3;
     private Enemy En3;
     @FXML
+    private CheckBox ENEMY3_ENABLE;
+    @FXML
     private Rectangle ENEMY4;
     private Enemy En4;
+    @FXML
+    private CheckBox ENEMY4_ENABLE;
     @FXML
     private Line UP_LINE;
     @FXML
@@ -53,17 +66,21 @@ public class Controller {
     @FXML
     private Text TIMER;
     private FloatProperty timeSeconds=new SimpleFloatProperty(0.f);
+    @FXML
+    private Text INTERSECTIONS;
+    private IntegerProperty intersectionCount=new SimpleIntegerProperty(0);
+
     private double GAME_AREA_WIDTH, GAME_AREA_HEIGHT;
     private Point2D GameAreaLocation;
 
-    private boolean isPaused=false;
+    private boolean isPaused=true;
+    private boolean isPlayerIntersect=false;
     private Timeline GameLoop;
 
     private EventHandler<MouseEvent> PlayerEvent= new EventHandler<MouseEvent>()
     {
         @Override
         public void handle(MouseEvent t) {
-            PLAYER.setFill(Color.RED);
             PLAYER.setTranslateX(t.getSceneX()-PLAYER.getWidth()/2);
             PLAYER.setTranslateY(t.getSceneY()-PLAYER.getHeight()/2);
         }
@@ -77,51 +94,130 @@ public class Controller {
     };
     @FXML
     private void CHANGE_PAUSE(ActionEvent event) {
-        isPaused=!isPaused;
-        if(isPaused)
-        {
-            PLAYER.setOnMouseDragged(PlayerPause);
-        }
-        else{
-            PLAYER.setOnMouseDragged(PlayerEvent);
+        if(ENEMY1_ENABLE.isSelected() || ENEMY2_ENABLE.isSelected() ||ENEMY3_ENABLE.isSelected() ||ENEMY4_ENABLE.isSelected()){
+            isPaused=!isPaused;
+            if(isPaused)
+            {
+
+                PLAYER.setOnMouseDragged(PlayerPause);
+            }
+            else{
+                timeSeconds.set(0.f);
+                PLAYER.setOnMouseDragged(PlayerEvent);
+            }
         }
     }
-
+    @FXML
+    private void SELECTED1(ActionEvent event){
+        En1.setEnabled(ENEMY1_ENABLE.isSelected());
+    }
+    @FXML
+    private void SELECTED2(ActionEvent event){
+        En2.setEnabled(ENEMY2_ENABLE.isSelected());
+    }
+    @FXML
+    private void SELECTED3(ActionEvent event){
+        En3.setEnabled(ENEMY3_ENABLE.isSelected());
+    }
+    @FXML
+    private void SELECTED4(ActionEvent event){
+        En4.setEnabled(ENEMY4_ENABLE.isSelected());
+    }
     @FXML
     public void initialize() {
         GAME_AREA_WIDTH=Math.abs(UP_LINE.getBoundsInParent().getMaxX()-UP_LINE.getBoundsInParent().getMinX());
         GAME_AREA_HEIGHT=Math.abs(RIGHT_LINE.getBoundsInParent().getMaxY()-RIGHT_LINE.getBoundsInParent().getMinY());
         GameAreaLocation=new Point2D(LEFT_LINE.getBoundsInParent().getMinX(),LEFT_LINE.getBoundsInParent().getMinY());
         PLAYER.setOnMouseDragged(PlayerEvent);
-        En1=new Enemy(20.0,20.0,100.0,ENEMY1);
-        En2=new Enemy(30.0,20.0,100.0,ENEMY2);
-        En3=new Enemy(70.0,20.0,100.0,ENEMY3);
-        En4=new Enemy(20.0,-20.0,100.0,ENEMY4);
+        En1=new Enemy(20.0,20.0,100.0, ENEMY1, ENEMY1.getBoundsInParent().getMinX(), ENEMY1.getBoundsInParent().getMinY(),false);
+        En2=new Enemy(30.0,20.0,100.0, ENEMY2, ENEMY2.getBoundsInParent().getMinX(), ENEMY2.getBoundsInParent().getMinY(),false);
+        En3=new Enemy(70.0,20.0,100.0, ENEMY3, ENEMY3.getBoundsInParent().getMinX(), ENEMY3.getBoundsInParent().getMinY(),false);
+        En4=new Enemy(20.0,-20.0,100.0,ENEMY4, ENEMY4.getBoundsInParent().getMinX(), ENEMY4.getBoundsInParent().getMinY(),false);
 
         GameLoop = new Timeline();
         GameLoop.setCycleCount(Timeline.INDEFINITE);
         GameLoop.getKeyFrames().add(new KeyFrame(Duration.seconds(0.0001), new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                CheckBoxRules();
                 if(!isPaused){Loop(0.0001f);}
             }
         }));
         GameLoop.playFromStart();
 
         TIMER.textProperty().bind(timeSeconds.asString());
+        INTERSECTIONS.textProperty().bind(intersectionCount.asString());
+    }
+    void CheckBoxRules(){
+        if(ENEMY1_ENABLE.isSelected())
+        {
+            if(!(ENEMY2_ENABLE.isSelected() || ENEMY3_ENABLE.isSelected() || ENEMY4_ENABLE.isSelected())){
+                ENEMY1_ENABLE.setDisable(true);
+            }
+            else
+                ENEMY1_ENABLE.setDisable(false);
+        }
+        if(ENEMY2_ENABLE.isSelected())
+        {
+            if(!(ENEMY1_ENABLE.isSelected() || ENEMY3_ENABLE.isSelected() || ENEMY4_ENABLE.isSelected())){
+                ENEMY2_ENABLE.setDisable(true);
+            }
+            else
+                ENEMY2_ENABLE.setDisable(false);
+        }
+        if(ENEMY3_ENABLE.isSelected())
+        {
+            if(!(ENEMY1_ENABLE.isSelected() || ENEMY2_ENABLE.isSelected() || ENEMY4_ENABLE.isSelected())){
+                ENEMY3_ENABLE.setDisable(true);
+            }
+            else
+                ENEMY3_ENABLE.setDisable(false);
+        }
+        if(ENEMY4_ENABLE.isSelected())
+        {
+            if(!(ENEMY1_ENABLE.isSelected() || ENEMY2_ENABLE.isSelected() || ENEMY3_ENABLE.isSelected())){
+                ENEMY4_ENABLE.setDisable(true);
+            }
+            else
+                ENEMY4_ENABLE.setDisable(false);
+        }
     }
 
     private class Enemy{
         Rectangle EnemyRec;
         Point2D SpeedVector;
         Double SpeedValue;
-        Enemy(Double SpeedVecX, Double SpeedVecY,Double Speed, Rectangle EnemyRectangle){
+        boolean isEnabled;
+        Point2D DefaultLocation;
+        Point2D SpawnLocation;
+        Enemy(Double SpeedVecX, Double SpeedVecY,Double Speed, Rectangle EnemyRectangle,Double DefaultLocX,Double DefaultLocY, boolean isEnable){
             EnemyRec=EnemyRectangle;
             SpeedVector=new Point2D(SpeedVecX,SpeedVecY);
+            DefaultLocation=new Point2D(DefaultLocX,DefaultLocY);
             normalize();
             SpeedValue=Speed;
+            isEnabled=isEnable;
         }
 
+        void setEnabled(boolean isEnable){
+            if(isEnable){
+                do{
+                    Double XCoord=ThreadLocalRandom.current().nextDouble(GameAreaLocation.getX(), GameAreaLocation.getX()+GAME_AREA_WIDTH + 1);
+                    Double YCoord=ThreadLocalRandom.current().nextDouble(GameAreaLocation.getY(), GameAreaLocation.getY()+GAME_AREA_HEIGHT + 1);
+                    EnemyRec.setTranslateY(YCoord);
+                    EnemyRec.setTranslateX(XCoord);
+                }while(EnemyRec.getBoundsInParent().intersects(PLAYER.getBoundsInParent()));
+                SpeedVector=new Point2D(ThreadLocalRandom.current().nextDouble(-100, 100 + 1),ThreadLocalRandom.current().nextDouble(-100,100 + 1));
+                normalize();
+                checkOnBound();
+            }
+            else
+            {
+                EnemyRec.setTranslateY(DefaultLocation.getY());
+                EnemyRec.setTranslateX(DefaultLocation.getX());
+            }
+            isEnabled=isEnable;
+        }
         void checkOnBound(){
             double translateX =  EnemyRec.getTranslateX();
             double translateY = EnemyRec.getTranslateY();
@@ -165,11 +261,14 @@ public class Controller {
             SpeedVector=new Point2D(SpeedVector.getX()/l,SpeedVector.getY()/l);
         }
         void move(float DeltaTime){
-            EnemyRec.setTranslateX(EnemyRec.getTranslateX()+SpeedVector.getX()*SpeedValue*DeltaTime);
-            EnemyRec.setTranslateY(EnemyRec.getTranslateY()+SpeedVector.getY()*SpeedValue*DeltaTime);
-            checkOnBound();
+            if(isEnabled){
+                EnemyRec.setTranslateX(EnemyRec.getTranslateX()+SpeedVector.getX()*SpeedValue*DeltaTime);
+                EnemyRec.setTranslateY(EnemyRec.getTranslateY()+SpeedVector.getY()*SpeedValue*DeltaTime);
+                checkOnBound();
+            }
         }
     }
+
     void checkPlayerOnBound(){
         double translateX = PLAYER.getTranslateX();
         double translateY = PLAYER.getTranslateY();
@@ -186,13 +285,34 @@ public class Controller {
             PLAYER.setTranslateX(GameAreaLocation.getX()+GAME_AREA_WIDTH-PLAYER.getWidth()-1);
         }
     }
+    void checkPlayerOnIntersection(){
+        Bounds PlayerBounds=PLAYER.getBoundsInParent();
+        Bounds En1Bounds=ENEMY1.getBoundsInParent();
+        Bounds En2Bounds=ENEMY2.getBoundsInParent();
+        Bounds En3Bounds=ENEMY3.getBoundsInParent();
+        Bounds En4Bounds=ENEMY4.getBoundsInParent();
+        if((PlayerBounds.intersects(En1Bounds) || PlayerBounds.intersects(En2Bounds) || PlayerBounds.intersects(En3Bounds) || PlayerBounds.intersects(En4Bounds))){
+            if(!isPlayerIntersect)
+            {
+                intersectionCount.set(intersectionCount.get()+1);
+                isPlayerIntersect=true;
+            }
+        }
+        else{
+            isPlayerIntersect=false;
+        }
+    }
     void Loop(float DeltaSeconds){
+
+
         checkPlayerOnBound();
-        PLAYER.setFill(Color.color(Math.random(), Math.random(), Math.random()));
+
         En1.move(DeltaSeconds);
         En2.move(DeltaSeconds);
         En3.move(DeltaSeconds);
         En4.move(DeltaSeconds);
         timeSeconds.set(timeSeconds.get()+DeltaSeconds);
+        checkPlayerOnIntersection();
+
     }
 }
